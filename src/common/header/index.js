70ca9029a6889229ef3fc2d1fd204c369713aa88
liujiana <liujiana@yy.com>
2020-09-11 14:49:13 +0800

    feat: actionCreators和constants的拆分

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -4,63 +4,41 @@ import {
   HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWrapper
 } from './style.js'
 import { CSSTransition } from 'react-transition-group'
-import * as actionCreators from './store/actionCreators'
+import { actionCreators } from './store'
 
-class Header extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      
-    }
-    this.handleInputFocus = this.handleInputFocus.bind(this)
-    this.handleInpuBlur = this.handleInpuBlur.bind(this)
-  }
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo></Logo>
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载app</NavItem>
-          <NavItem className="right">
-            <i className="iconfont">&#xe636;</i>
-          </NavItem>
-          <NavItem className="right">登陆</NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              in={this.props.focused}
-              timeout={200}
-              classNames="slide">
-              <NavSearch  className={this.props.focused ? 'focused': ''}
-                          onFocus={this.props.handleInputFocus}
-                          onBlur={this.props.handleInpuBlur}
-              ></NavSearch>
-            </CSSTransition>
-            <i className={this.props.focused ? 'iconfont focused': 'iconfont'}>&#xe650;</i>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className="writing">
-            <i className="iconfont">&#xe601;</i>
-            写文章
-          </Button>
-          <Button className="reg">注册</Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
-
-  handleInputFocus() {
-    this.setState({
-      focused: true
-    })
-  }
-
-  handleInpuBlur() {
-    this.setState({
-      focused: false
-    })
-  }
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo></Logo>
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载app</NavItem>
+        <NavItem className="right">
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <NavItem className="right">登陆</NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.focused}
+            timeout={200}
+            classNames="slide">
+            <NavSearch  className={props.focused ? 'focused': ''}
+                        onFocus={props.handleInputFocus}
+                        onBlur={props.handleInpuBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <i className={props.focused ? 'iconfont focused': 'iconfont'}>&#xe650;</i>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className="writing">
+          <i className="iconfont">&#xe601;</i>
+          写文章
+        </Button>
+        <Button className="reg">注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
 }
 
 const mapStateToProps = (state) => {
